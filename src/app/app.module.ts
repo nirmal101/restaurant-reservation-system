e57b8b378cefaa33ce000e82ce30fd9c1143bf61
nisaldesilva7 <nisaldesilva7@gmail.com>
2020-08-06 18:21:49 +0530

    UI template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    TeamComponent,
+    MenuComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
